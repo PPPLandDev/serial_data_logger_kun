f1d6d07224e513b8081e4bfad007973388a3d8fa
yakinikunakamura <mail.keisuke.nakamura01234@gmail.com>
2021-05-02 18:09:57 +0900

    Initial commit

---
+++ src/main/java/serialdatalogger/LoggerMainFrame.java
@@ -0,0 +1,49 @@
+package serialdatalogger;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * シリアルデータ通信プログラム<br>
+ *
+ * 【更新履歴】<br>
+ * <li>Rev01.00 2021-05-02 : 新規作成 			K.Nakamura<br>
+ *
+ */
+public class LoggerMainFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoggerMainFrame frame = new LoggerMainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoggerMainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
